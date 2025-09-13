40e6c87fe7d6e944761c00334bb50ec78dbe5188
feldyb <68964118+feldyb@users.noreply.github.com>
2025-09-13 15:46:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,9 +17,4 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/aaronbates/project-guidelines/tree/9861cbf0f6b95151e21d43a2ed8ce0047d3ddb7b/html%2Fhtml-guidelines.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/NotTodayMuggleFucker/Bedu/tree/5f4401220ff5768d3380f0fadbc401b28759404c/Courses%2FC1-React-2020-master%2FBuenasPracticas%2FPWA%2FReadme.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/MSeptianJ/Footbal_Club/tree/4028f74b99159467dbcd2c8ce9d23bf7a569c03b/service-worker.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "3")
